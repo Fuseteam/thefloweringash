0fb67563ecfa48769c30a567d5bb8be637ca6df2
Merkhad Luigton <fusekai@outlook.com>
2020-11-30 09:57:21 -0300

    fix missing variable

--- default.nix
+++ default.nix
@@ -4,6 +4,10 @@ let
     url = "https://github.com/Fuseteam/nixpkgs";
     rev = "40c44fdd019cb96f2989818516a2aed4cb7e3464";
   };
+  pkgsOriginal = import nixpkgs {
+    overlays = [
+    ];
+  };
   pkgsCross = pkgsOriginal.pkgsCross.${target};
   in with pkgsCross; [
   vim
