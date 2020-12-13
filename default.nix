4d34952c0482588c05df1ae302fd790f6ba30c60
Merkhad Luigton <fusekai@outlook.com>
2020-12-13 02:33:33 +0000

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {target, package}:
 let
-   nixpkgs= builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/58cb5383869ced196b0b198a78daca08b63e958e.tar.gz";
+   nixpkgs= builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/cd63096d6d887d689543a0b97743d28995bc9bc3.tar.gz";
    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   };
   pkgsOriginal = import nixpkgs {
